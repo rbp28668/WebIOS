a3ef1641822645ed976dd38535889bfc2ab31fba
Bruce Porteous <bruce.porteous@ntlworld.com>
2020-12-21 15:45:20 +0000

    improved slew and fixed igc traffic repeat bug

--- src/app/recording/recording.component.ts
+++ src/app/recording/recording.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { P3dcmdService} from '../p3dcmd.service';
 
 @Component({
   selector: 'app-recording',
@@ -7,9 +8,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecordingComponent implements OnInit {
 
-  constructor() { }
+  private recordings : Array<String> = [];
 
-  ngOnInit() {
+  constructor(private p3dcmd: P3dcmdService) { }
+
+
+  private logFailure(data) : void {
+    console.log("Failed: " + data.reason + data.code);
   }
+  
+   // Initialise the model.
+   ngOnInit() : void {
+    console.log("Recording component ngOnInit");
+    this.loadRecordings();
+   }
+
+  private loadRecordings() : void {
+    this.p3dcmd.recordList().subscribe( data => {
+      // Process resulting list
+      if(data.status === "OK") {
+        data.recordings.forEach( (item: String) => {
+          this.recordings.push(item);
+        });
+
+      } else { // failed
+        this.logFailure(data);
+      }
+    }, (err: any) => { // on error
+      console.log("Error fetching recordings");
+      console.log(err);
+    });
+  }
+
+
+  analyse(): void {
+    this.p3dcmd.recordAnalyse().subscribe( data => {
+      if(data.status !== "OK") {
+        this.logFailure(data);
+      }
+    }, (err: any) => { // on error
+      console.log("Error fetching scenarios");
+      console.log(err);
+    });
+  }
+
+  getRecordings() : Array<String> {
+    return this.recordings;
+  }
+
+
+  playRecording(name : String): void {
+    this.p3dcmd.recordPlay(name).subscribe( data => {
+      if(data.status !== "OK") {
+        this.logFailure(data);
+      }
+    }, (err: any) => { // on error
+      console.log("Error playing recording");
+      console.log(err);
+    });
+  }
+
+  startRecording(): void {
+    this.p3dcmd.recordStart().subscribe( data => {
+      if(data.status !== "OK") {
+        this.logFailure(data);
+      }
+    }, (err: any) => { // on error
+      console.log("Error starting recording");
+      console.log(err);
+    });
+  }
+  
+  stopRecording(title : String, description : String): void {
+    this.p3dcmd.recordStop(title, description).subscribe( data => {
+      if(data.status !== "OK") {
+        this.logFailure(data);
+      }
+    }, (err: any) => { // on error
+      console.log("Error playing recording");
+      console.log(err);
+    });
+  }
+
 
 }
